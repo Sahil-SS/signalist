d6166944e1e55c14cafa6123d8c389aaa3749708
Sahil Sinha <sinhasahil272004@gmail.com>
2025-11-24 16:53:00 +0530

    fixed type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "StockWhiz",
+  title: "Signalist",
   description: "AI-Powered Investment Research for Smarter Decisions.",
 };
 
